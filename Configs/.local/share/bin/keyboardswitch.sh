8e5b4af1cde9bacefd21617208501cf86da13fcb
Evren-os <avrenosenpai@gmail.com>
2024-09-14 04:30:39 +0600

    merge changes from hyprdots

--- Configs/.local/share/bin/keyboardswitch.sh
+++ Configs/.local/share/bin/keyboardswitch.sh
@@ -7,3 +7,4 @@ hyprctl switchxkblayout all next
 
 layMain=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
 notify-send -a "t1" -r 91190 -t 800 -i "~/.config/dunst/icons/keyboard.svg" "${layMain}"
+
